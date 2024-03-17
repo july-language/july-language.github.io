6db9abf2ec150788eba2614f28adaff28258c429
大石樹 <itsuki.oishi0402@gmail.com>
2024-03-17 17:41:02 +0900

    a

---
+++ function.js
@@ -0,0 +1,8 @@
+<div id="result"></div>
+
+<script>
+  function displayText() {
+    var inputText = document.getElementById("inputText").value;
+    document.getElementById("result").innerHTML = inputText;
+  }
+</script>
